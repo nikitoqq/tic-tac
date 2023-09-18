52f15be144cf73b41c45893bfc21866adfcd8b75
Philipp <ghotfrid@ghotfrid.com>
2023-09-19 00:57:08 +0300

    review

--- src/components/square/buttonSquare/BtnSquare.jsx
+++ src/components/square/buttonSquare/BtnSquare.jsx
@@ -2,6 +2,11 @@
 import CheckWin from '../../../logic/winPos';
 import React, { useState } from 'react';
 import './BtnSquare.css';
+// название папки должно быть с большой буквы + совпадать с названием файла
+// файл Square.jsx нужно прокинуть в папку Square и назвать index.jsx
+// like
+// components/Square/index.jsx 
+// сейм фор ButtonSquare
 
 const matrix = {
     i1: '', i2: '', i3: '',
@@ -11,11 +16,17 @@ const matrix = {
 
 function BtnSquare(props) {
 
+    // здесь можно заменить на дестуктуризацию
+    // like
+    // const {id, countProps} = props;
+
     const { id } = props;
+    // имя лучше поменять на что-то другое (без props)
     const { countProps } = props;
 
     const [Text, SetText] = useState('');
 
+    // с маленькой буквы и стрелочной функцией (а еще лучше через useCallback)
     function Click(event) {
 
         const target = event.target;
@@ -29,6 +40,7 @@ function BtnSquare(props) {
             SetSymble();
         }
 
+        // с маленькой буквы и стрелочной функцией (а еще лучше через useCallback) и функцию внутри функции низя объявлять
         function SetSymble() {
             SetText(symble);
             matrix[target.id] = symble;
